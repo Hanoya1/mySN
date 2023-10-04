a2a41e0eadbf2b1bda81f054bac7164f121a4c9e
Matvei <leshinskymatvey@gmail.com>
2023-10-04 19:11:31 +0200

    First commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+   return (
+      <header className='header'>
+         <img src='https://seeklogo.com/images/P/Praha-logo-D9CC980EDF-seeklogo.com.png' alt='mySN'></img>
+      </header>
+   )
+}
+
+export default Header
---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+   return (
+      <div className='content'>
+         <img src='https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg' alt='main img'></img>
+         <div>
+            ava + desc
+         </div>
+         <div>
+            My posts
+            <div>
+               New post
+            </div>
+            <div> post 1</div>
+            <div> post 2</div>
+         </div>
+      </div>
+   )
+}
+
+export default Profile

4aa60d6923b130b0683b784365e6cbebb170a7c3
Matvei <leshinskymatvey@gmail.com>
2023-10-06 14:04:33 +0200

    add css modules

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
    return (
-      <header className='header'>
+      <header className={s.header}>
          <img src='https://seeklogo.com/images/P/Praha-logo-D9CC980EDF-seeklogo.com.png' alt='mySN'></img>
       </header>
    )
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css'
 
 const Profile = () => {
    return (
-      <div className='content'>
+      <div className={s.content}>
          <img src='https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg' alt='main img'></img>
          <div>
             ava + desc
@@ -12,8 +13,10 @@ const Profile = () => {
             <div>
                New post
             </div>
-            <div> post 1</div>
-            <div> post 2</div>
+            <div className={s.content}>
+               <div className={s.item}> post 1</div>
+               <div className={s.item}> post 2</div>
+            </div>
          </div>
       </div>
    )

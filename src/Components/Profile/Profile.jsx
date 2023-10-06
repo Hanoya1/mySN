1810681642c95c348d7acf9144373f8ac43f497f
Matvei <leshinskymatvey@gmail.com>
2023-10-06 15:11:39 +0200

    add MyPosts and Post

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+
 
 const Profile = () => {
    return (
@@ -8,16 +10,7 @@ const Profile = () => {
          <div>
             ava + desc
          </div>
-         <div>
-            My posts
-            <div>
-               New post
-            </div>
-            <div className={s.content}>
-               <div className={s.item}> post 1</div>
-               <div className={s.item}> post 2</div>
-            </div>
-         </div>
+         <MyPosts />
       </div>
    )
 }

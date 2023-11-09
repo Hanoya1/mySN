b3c1bfb30d51e4c945af84c3dc7ba96f9252ed4d
hero hero <leshinskymatvey@gmail.com>
2023-11-09 14:50:05 +0100

    prepare files to create props

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
    return (
       <div className={s.dialogs}>
          <div className={s.dialogsItems}>
             <div className={s.dialog + ' ' + s.active}>
-               Roman
+               <NavLink to='/1'>Roman</NavLink>
             </div>
             <div className={s.dialog}>
-               Roman
+               <NavLink to='/2'>Roman</NavLink>
             </div>
             <div className={s.dialog}>
-               Roman
+               <NavLink to='/3'>Roman</NavLink>
             </div>
             <div className={s.dialog}>
-               Roman
+               <NavLink to='/4'>Roman</NavLink>
             </div>
          </div>
          <div className={s.messages}>
@@ -26,5 +27,4 @@ const Dialogs = () => {
       </div>
    )
 }
-
 export default Dialogs

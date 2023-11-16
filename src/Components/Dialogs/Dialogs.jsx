c3b7c7deb5f49c6cb7f1f34ee32dddd7b5c06fe3
hero hero <leshinskymatvey@gmail.com>
2023-11-16 14:08:09 +0100

    add Message component + start working with props

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,35 +2,14 @@ import React from "react";
 import s from './Dialogs.module.css'
 import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
-
-const Message = (props) => {
-   return (
-      <div>{props.message}</div>
-   )
-}
-
-
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-   let dialogsItems = [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Dimych' },
-      { id: 3, name: 'Dimych' },
-      { id: 4, name: 'Dimych' },
-      { id: 5, name: 'Dimych' },
-   ]
 
-   let dialogsElements = dialogsItems.map(d => <DialogItem name={d.name} id={d.id} />);
+   let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
 
-   let messagesItems = [
-      { message: "I'll do it!!!" },
-      { message: "I'll do it!!!" },
-      { message: "I'll do it!!!" },
-      { message: "I'll do it!!!" },
-      { message: "I'll do it!!!" },
-   ]
+   let messagesElements = props.messages.map(m => <Message className={s.message} message={m.message} />)
 
-   let messagesElements = messagesItems.map(m => <Message className={s.message} message={m.message} />)
    return (
       <div className={s.dialogs}>
          <div className={s.dialogsItems}>
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import Post from "./Post/Post";
 import s from './MyPosts.module.css'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-   let postsData = [
-      { message: "Hi, im trying to make my first project", likesCount: '120' },
-      { message: "Hi, sounds great!", likesCount: '11' }
-   ]
 
-   let postsElements = postsData.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+   let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
    return (
       <div>
